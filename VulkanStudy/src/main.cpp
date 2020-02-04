4fea156707f303bb954bb161742294d726eb5f96
wyt <as5818956@gmail.com>
2020-02-04 10:20:32 +0900

    1

--- VulkanStudy/src/main.cpp
+++ VulkanStudy/src/main.cpp
@@ -1,37 +1,45 @@
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
 #include <vulkan/vulkan.h>
 
 #include <iostream>
+#include <stdexcept>
+#include <functional>
+#include <cstdlib>
 
-int main() {
-	glfwInit();
-
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-	uint32_t extensionCount = 0;
-	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-	std::cout << extensionCount << " extensions supported" << std::endl;
-
-	glm::mat4 matrix;
-	glm::vec4 vec;
-	auto test = matrix * vec;
-
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
+class Application
+{
+public:
+	void Run()
+	{
+		InitVulkan();
+		MainLoop();
+		Cleanup();
 	}
 
-	glfwDestroyWindow(window);
+private:
+	void InitVulkan(){
+	
+	}
 
-	glfwTerminate();
+	void MainLoop() {
 
-	return 0;
+	}
+
+	void Cleanup() {
+
+	}
+};
+
+int main()
+{
+	Application app;
+	
+	try {
+		app.Run();
+	}
+	catch (const std::exception & e)
+	{
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
 }
